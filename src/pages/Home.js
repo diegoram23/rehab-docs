35379a45457ebdbe3897b62e1e559bf2cda020e2
diegoram23 <diego.pta.oc@gmail.com>
2023-04-02 17:38:38 -0700

    setup gh-pages

---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+const Home = () => {
+    return (
+        <div className="patient-form">
+            <h2>Add a new patient</h2>
+            <form>
+                <label>
+                    <span>First name:</span>
+                    <input
+                        required
+                        type='text'
+                    />
+                </label>
+                <label>
+                    <span>Last name:</span>
+                    <input
+                        required
+                        type='text'
+                    />
+                </label>
+                <label>
+                    <span>DOB:</span>
+                    <input
+                        required
+                        type='text'
+                    />
+                </label>
+                <label>
+                    <span>Diagnosis:</span>
+                    <input
+                        required
+                        type='text'
+                    />
+                </label>
+                <button>Add patient</button>
+            </form>
+        </div>
+    );
+}
+
+export default Home;
