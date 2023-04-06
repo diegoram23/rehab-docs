3d74398b6c726c84ec78da98675de7cb8fcb43b8
diegoram23 <diego.pta.oc@gmail.com>
2023-04-06 12:19:37 -0700

    styling and minor bug fixes to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,44 @@
 
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import { db } from "../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Modal = ({ singlePatient }) => {
 
-    const [modal, setModal] = useState(true)
+    const [modal, setModal] = useState(false)
 
-    const toggleModal = (id) => {
+    const navigate = useNavigate()
+
+    //Function to remove patient from db and frontend
+    const deletePatient = async (id) => {
+        //closes modal
         setModal(!modal)
-        console.log('id', id)
-        console.log('modal', modal)
+
+        const docRef = doc(db, 'patients', id)
+        await deleteDoc(docRef)
+
+        //sends user to homepage
+        navigate('/')
     }
+
 
     return (
         <>
-            <button className="delete-btn" onClick={() => toggleModal(singlePatient[0].id)}>Delete patient caseload</button>
+            <button className="delete-btn" onClick={() => setModal(!modal)}>Delete patient caseload</button>
+
+            {/*Modal only appears if true */}
+
             {modal && <div className="modal">
                 <div className="overlay"></div>
                 <div className="modal-content">
-                    <p>{`Are you sure you want to remove ${singlePatient[0].first} ${singlePatient[0].last} from caseload?`}</p>
+                    <p>{`Are you sure you want to remove '${singlePatient[0].first} ${singlePatient[0].last}' from caseload?`}</p>
                     <div className="modal-btns-container">
-                        <button className="modal-btn">No</button>
-                        <button className="modal-btn">Yes</button>
+
+                        {/*Clicking no closes modal */}
+                        <button className="modal-btn" onClick={() => setModal(!modal)}>No</button>
+                        {/*Clicking yes calls function to delete user */}
+                        <button className="modal-btn" onClick={() => deletePatient(singlePatient[0].id)}>Yes</button>
                     </div>
                 </div>
             </div>}
