4056e6cd5aae1b2f411c94ea1319cec1947ce73a
diegoram23 <diego.pta.oc@gmail.com>
2023-04-18 12:53:33 -0700

    added a href to footer sends user to github page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ const Footer = () => {
         <footer>
             <h2>Rehab<span>Docs</span></h2>
             <section className="contact-footer">
-            <p>Contact Us</p>
-            <p>Tech Support</p>
-            <p>Terms of Service</p>
+                <p>Contact Us</p>
+                <p>Tech Support</p>
+                <p>Terms of Service</p>
             </section>
-            <p className="last">&#169; github.com/diegoram23</p>
+            <p className="last">&#169; <a href="https://github.com/diegoram23">github.com/diegoram23</a></p>
 
         </footer>
     );
